85ed68fe5614fe7668793214fb606431c561dd6a
BibiaiAgency <121713728+BibiaiAgency@users.noreply.github.com>
2023-05-22 02:35:11 +0200

    Archivos correctos

---
+++ src/Plugin/Block/SeguraViudasPasswordrecovery.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\blocks_segura_viudas\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a custom block for Segura Viudas Passwordrecovery.
+ *
+ * @Block(
+ *   id = "segura_viudas_passwordrecovery",
+ *   admin_label = @Translation("Segura Viudas Passwordrecovery"),
+ *   category = @Translation("Custom")
+ * )
+ */
+class SeguraViudasPasswordrecovery extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The form builder.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * Constructs a new SeguraViudasPasswordrecovery block.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Form\FormBuilderInterface $form_builder
+   *   The form builder.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $form_builder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $form_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $form = $this->formBuilder->getForm('Drupal\blocks_segura_viudas\Form\PasswordRecoveryForm');
+
+    return [
+      '#theme' => 'segura_viudas_passwordrecovery',
+      '#content' => [
+        'form' => $form,
+      ],
+    ];
+  }
+
+}
